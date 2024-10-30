6526989d5df1ed119c42cdad70e4e8811397d450
Jesper Sørensen <jeppester1@gmail.com>
2024-10-30 12:15:50 +0000

    Styling: Ensure that tailwind util take precedence over component classes

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+import { cascadeLayerPrefixer } from './utils/cascade_layer_prefixer.js'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    tailwindcss,
+    cascadeLayerPrefixer({
+      layerName: 'components',
+      fileNameMatcher: /\/components\/.+/,
+    }),
+    autoprefixer,
+  ],
 }
