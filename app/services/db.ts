9f0ab61d16a3f2e45347be68af7f759267227e9e
Jesper Sørensen <jeppester1@gmail.com>
2025-03-26 19:55:50 +0000

    db: Improve withGlobalTransaction to allow for transactions in source code

--- app/services/db.ts
+++ app/services/db.ts
@@ -1,4 +1,4 @@
-import { CamelCasePlugin, Kysely, PostgresDialect } from 'kysely'
+import { CamelCasePlugin, Kysely, PostgresDialect, sql } from 'kysely'
 import PG from 'pg'
 import { DB } from '#database/types'
 import env from '#start/env'
@@ -29,7 +29,33 @@ export const withGlobalTransaction = () => {
     globalDb
       .transaction()
       .execute((transaction) => {
-        globalTransaction = transaction
+        globalTransaction = new Proxy(transaction, {
+          get: (target, prop) => {
+            // To prevent transactions in source code from failing,
+            // emulate transaction functionality with save points.
+            //
+            // Based on this example:
+            // https://github.com/kysely-org/kysely/issues/255#issuecomment-1616684003
+            /* v8 ignore start */
+            if (prop === 'transaction') {
+              return () => ({
+                execute: async (cb: (kyselyTx: Kysely<DB>) => Promise<Kysely<DB>>) => {
+                  await sql`SAVEPOINT test;`.execute(transaction)
+
+                  return cb(transaction).catch(async () => {
+                    // on explicit reject, rollback to savepoint
+                    sql`ROLLBACK TO SAVEPOINT test;`.execute(transaction)
+                  })
+                },
+              })
+            }
+            /* v8 ignore end */
+
+            // @ts-expect-error Property access, keys not inferred
+            const attr = target[prop]
+            return typeof attr === 'function' ? attr.bind(target) : attr
+          },
+        })
 
         return new Promise((_res, rej) => {
           rollback = () => {
