00275fb6faf7d648cfa416b2f7ed6a476542a72f
Jesper Sørensen <jeppester1@gmail.com>
2026-01-05 09:13:41 +0100

    BasicAuthMiddleware: Prevent timing attacks

--- app/middleware/basic_auth_middleware.ts
+++ app/middleware/basic_auth_middleware.ts
@@ -1,5 +1,6 @@
 import { errors, type HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
+import { createHmac, randomBytes, timingSafeEqual } from 'node:crypto'
 
 /**
  * BasicAuth middleware is used to protect routes with HTTP Basic Authentication.
@@ -15,9 +16,12 @@ export default class BasicAuthMiddleware {
   ) {
     const authHeader = ctx.request.header('authorization') ?? ''
     const base64Credentials = authHeader.slice(6)
-
-    const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8')
-    if (credentials === `${options.user}:${options.password}`) return next()
+    const credentials = Buffer.from(base64Credentials, 'base64')
+    const expected = Buffer.from(`${options.user}:${options.password}`)
+    const key = randomBytes(32)
+    const hmacUser = createHmac('sha256', key).update(credentials).digest()
+    const hmacSecret = createHmac('sha256', key).update(expected).digest()
+    if (timingSafeEqual(hmacUser, hmacSecret)) return next()
 
     ctx.response.header('WWW-Authenticate', 'Basic realm="Restricted Area"')
     throw new errors.E_HTTP_EXCEPTION('Unauthorized', { status: 401 })
